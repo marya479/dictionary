f8dfa23aeb77b4d585decdc5627927af7a037f69
marya479 <m.weglinska479@gmail.com>
2024-06-11 21:25:00 +0200

    API added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary(){
  let [keyword, setKeyword] = useState("");
  
+function handleResponse(response) {
+console.log(response.data[0]);
+}
+
     function search(event){
     event.preventDefault();
-    alert('Searching for ${keyword} definition');
+    
 
+    let apiUrl = 'https://api.shecodes.io/dictionary/v1/define?word={word}&key=829fbdd0ctf5931e48b08422o27faf86'
  }
+ axios.get(apiUrl).then(handleResponse) }
 function handleKeywordChange(event) {
     
     setKeyword(event.target.value);
