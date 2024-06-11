23e8b5a946f9c6628219bcd8713f8536991b2dd8
marya479 <m.weglinska479@gmail.com>
2024-06-11 21:09:25 +0200

    search engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <div className="App"> <div className="container">
       <header className="App-header">
 <img src={logo} className="App-logo img-fluid" alt="logo" />
         
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">Coded by Marta Lucja</footer>
+      </div>
     </div>
   );
 }
 
-export default App;
+
