a4a9840b1fb2b7726d59b03509d4a0f8c63e8c84
marya479 <m.weglinska479@gmail.com>
2024-06-12 08:42:44 +0200

    return again

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results.js";
 
 export default function Dictionary(){
  let [keyword, setKeyword] = useState("");
+ let [results, setResults] = useState({});
  
 function handleResponse(response) {
 console.log(response.data[0]);
+setResults(response.data[0]);
 }
 
     function search(event){
@@ -25,5 +28,7 @@ function handleKeywordChange(event) {
         <form onSubmit={search}>
             <input type="search" onChange={handleKeywordChange} />
         </form>
-     </div>
- )
+     < Results />   
+    </div>
+   )
+}
