168152e79162675c17e6970f20d5ccb1770e5657
marya479 <m.weglinska479@gmail.com>
2024-06-14 20:42:22 +0200

    links

--- src/App.js
+++ src/App.js
@@ -12,8 +12,31 @@ export default function App() {
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
-        <footer className="App-footer">
-          <small>Coded by SheCodes</small>
+        <footer>
+          This project was coded by Marta Lucja
+          <a
+            href="https://www.linkedin.com/in/marta-lucja-weglinska-761603211/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            SheCodes
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/marya479/dictionary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://grand-youtiao-28f063.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
         </footer>
       </div>
     </div>
