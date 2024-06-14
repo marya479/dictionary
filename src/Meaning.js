e48ea9e8cc68f545e1c5ebc261d93298f4aa4507
marya479 <m.weglinska479@gmail.com>
2024-06-14 20:39:14 +0200

    all

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,24 +1,21 @@
 import React from "react";
+import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-    console.log(props.meaning);
-    return (
-        <div className="Meaning">
-            <h3>{props.meaning.partOfSpeech}</h3>
-            {props.meaning.definitions.map(function (definition, index)
-        {
-return(
-  <div key={index}>
-    <p>
-        {definition.definition}
-        <br />
-        <em>{definition.example}</em>
-    </p>
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <div className="definition">{definition.definition}</div>
+
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+        );
+      })}
     </div>
-
-);
-
-        })}
-</div>
-    );
+  );
 }
